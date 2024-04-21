bb4bc8347fbb32909722120a1884f029e3093b49
Timo Stülten <8837182+stuelten@users.noreply.github.com>
2024-04-21 23:04:05 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    mitTon = 1
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    mitTon = 0
+})
+let mitTon = 0
+basic.showIcon(IconNames.Heart, 1000)
 basic.showLeds(`
     . # # # .
     # . # . #
@@ -6,10 +14,15 @@ basic.showLeds(`
     . # # # .
     `)
 basic.forever(function () {
-    basic.showCompass(50)
+    basic.showCompass(10)
     if (input.compassHeading() < 10 || input.compassHeading() > 350) {
         basic.setLedColors(0x00ff00, 0x00ff00, 0x00ff00)
     } else {
         basic.setLedColors(0xff0000, 0xff0000, 0xff0000)
+    }
+    if (mitTon > 0) {
+        music.ringTone(500 + input.compassHeading())
+    } else {
+        music.stopAllSounds()
     }
 })
